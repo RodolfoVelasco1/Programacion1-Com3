6f8fec4e05861bc64eb7e50771f0525bb4f345de
“RodolfoVelasco1” <rudyvelasco96@gmail.com>
2023-08-16 22:39:21 -0300

    Primer commit - Ejercicios Complementarios

---
+++ ej_complementario.py
@@ -0,0 +1,89 @@
+#Alumno: Rodolfo Nicolás Velasco Fessler
+#Ejercicio 1
+
+numero1 = 30
+
+#Ejercicio 2
+
+numero2 = 0.5
+
+#Ejercicio 3
+
+suma = numero1 + numero2
+print(suma)
+
+#Ejercicio 4
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print(f"La resta da {resta}. La multiplicación da {multiplicacion}. La división da {division}.")
+
+#Ejercicio 5
+
+nombre = "Rodolfo"
+print(nombre)
+
+#Ejercicio 6 
+
+precio = 20.50
+
+#Ejercicio 7
+
+descuento = 0.5
+
+#Ejercicio 8
+
+precio_final = precio * descuento
+print(precio_final)
+
+#Ejercicio 9
+
+cadena = "Programar es lo mejor"
+print(cadena)
+
+#Ejercicio 10
+
+longitud = len(cadena)
+print(longitud)
+
+#Ejercicio 11
+
+precio = 6.7
+print(precio)
+precio_entero = int(precio)
+print(precio_entero)
+
+#Ejercicio 12
+
+nombre = "Rodolfo"
+apellido = "Velasco"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+#Ejercicio 13
+
+edad = 27
+print(edad)
+edad = edad + 5
+print(edad)
+edad = edad - 10
+print(edad)
+
+#Ejercicio 14
+
+altura = 1.7
+print(altura)
+altura = altura * 4
+print(altura)
+altura = altura / 3
+print(altura)
+
+#Ejercicio 15
+
+nombre_mayus = "RODOLFO"
+print(nombre_mayus.lower())
+
+#Ejercicio 16
+nombre_mayus = "RODOLFO"
+print(nombre_mayus.capitalize())
