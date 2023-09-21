07c6151d55c082e8ec534ea51b584b6fd5a56560
“RodolfoVelasco1” <rudyvelasco96@gmail.com>
2023-09-21 09:15:59 -0300

    Funciones Ejercicio 1 - Rodolfo Velasco

---
+++ funciones_ej1.py
@@ -0,0 +1,23 @@
+﻿#Alumno: Rodolfo Nicolás Velasco Fessler
+#Mi repositorio en GitHub: https://github.com/RodolfoVelasco1/Programacion1-Com3
+
+#DEFINICIÓN DE FUNCIONES
+
+def most(a, b):
+    if (a > b):
+        return a
+    else:
+        return b
+
+def least(a, b):
+    if (a < b):
+        return a
+    else:
+        return b
+
+#PROGRAMA PRINCIPAL
+
+x = int(input("Ingrese un número: "))
+y = int(input("Ingrese otro número: "))
+
+print(most(x - 3, least(x + 2, y - 5)))
